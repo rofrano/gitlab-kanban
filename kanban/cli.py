e92d9f06f1216e3b77c012b2b5ab0526befc88a3
John Rofrano <johnnyroy@johnrofrano.com>
2022-06-03 11:41:19 +0000

    Added copyright

--- kanban/cli.py
+++ kanban/cli.py
@@ -1,5 +1,26 @@
+######################################################################
+# Copyright 2022 John Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
 """
 GitLab Kanban Board Command Line Interface
+
+This module provides a command line interface to create Kanban boards in GitLab.
+Since Gitlab Kanban boards are created using labels, it has the ability to
+create labels as well. Finally it can create Issues to populate a Kanban board
+from a Comma Separated Value (CSV) file.
 """
 import csv
 import click
@@ -28,7 +49,7 @@ def cli(ctx, token, project, gitlab_url):
 @cli.group()
 @click.pass_context
 def labels(ctx):
-    """Create, Get, Update, Delete Labels"""
+    """Create, list, update, delete Labels"""
 
 #---------------------------------------------------------------------
 # CREATE LABELS
@@ -87,7 +108,7 @@ def delete_labels(ctx, input):
 @cli.group()
 @click.pass_context
 def boards(ctx):
-    """Create, Get, Update, Delete Kanban Boards"""
+    """Create, list, update, delete Kanban Boards"""
 
 #---------------------------------------------------------------------
 # LIST BOARDS
@@ -164,7 +185,7 @@ def delete_boards(ctx, name):
 @cli.group()
 @click.pass_context
 def issues(ctx):
-    """Create, Get, Update, Delete Issues"""
+    """Create, list, update, delete Issues"""
 
 #---------------------------------------------------------------------
 # CREATE ISSUES
